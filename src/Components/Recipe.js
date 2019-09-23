a2c5675baee78db47106f840ba3dace491c30ac2
Pranav <pnarang1130@gmail.com>
2019-09-23 13:39:01 -0700

    setup links

--- src/Components/Recipe.js
+++ src/Components/Recipe.js
@@ -1,7 +1,30 @@
 import React from 'react';
 
-const Recipe = () => (
-    <div>Recipe</div>
-);
+class Recipe extends React.Component{
+
+    componentDidMount = () => {
+        e.preventDefault();
+        const API_KEY = '3341a283cb3aaca931c2a5f74d4ff355';
+
+        recipeName = this.props.location.state.recipe;
+        const API_CALL = await fetch(`https://www.food2fork.com/api/search?key=${API_KEY}&q=${recipeName}`);
+    
+        const response = await API_CALL.json();
+    
+        this.setState({
+          activeRecipe: response.recipes[0]
+        });
+    }
+
+    state = {
+        activeRecipe: []
+    }
+
+    render(){
+        return(
+            <div>hi</div>
+        );
+    }
+}
 
 export default Recipe;
--- src/Components/Recipes.js
+++ src/Components/Recipes.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Recipes = (props) => (
     <div className="container">
@@ -22,7 +23,17 @@ const Recipes = (props) => (
                                 </span>
                             </p>
                         </div>
-                        <button className="recipe_buttons">View Recipe</button>
+                        <button className="recipe_buttons">
+                            <Link 
+                            to={{
+                                pathname = `recipe/${recipe.recipe_id}`,
+                                state = {
+                                    recipe: recipe.title,
+                                }
+                            }}
+                            
+                            >View Recipe</Link> 
+                        </button>
                     </div>
                 );
             })
